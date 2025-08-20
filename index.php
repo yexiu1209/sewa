645d6b5552d47c094b61fecb9e0e76b3a218e790
yexiu1209 <rasyaseptianramadhan1@gmail.com>
2025-08-20 16:12:58 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+include(koneksi.php)
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+          <meta charset="UTF-8">https://github.com/yexiu1209/sewa/tree/main
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <title>Document</title>
+</head>
+<body>
+          
+</body>
+</html>
