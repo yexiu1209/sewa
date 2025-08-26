9f4aeb9eb0874050f1ac8faaacc6f4239cd0c49a
yexiu1209 <rasyaseptianramadhan1@gmail.com>
2025-08-26 14:43:21 +0700

    asdsa

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
+<?php
+include("koneksi.php");
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,6 +11,39 @@
           <title>Document</title>
 </head>
 <body>
-selamat datang di halaman ini asdasdasdsa
+          <table>
+                    <tr>
+                              <td>no</td>
+                              <td>Nama Barang</td>
+                              <td>Harga</td>
+                              <td>Stok</td>
+                              <td>Tanggal Masuk</td>
+                              <td>Kondisi</td>
+                              <td>Status</td>
+                              <td>Jenis Barang</td>
+                              <td>Action</td>
+                    </tr>
+                    <tr>
+                              <?php
+                             $no = 1;
+                             $query = "SELECT * FROM barang JOIN jenis_barang ON barang.jenis_barang = jenis_barang.id ";
+                             $sql = mysqli_query($koneksi, $query);
+                             while ($row = mysqli_fetch_array($sql)) {
+                             ?>
+                              <td><?=$no?></td>
+                              <td><?=$row['nama_barang']?></td>
+                              <td></td>
+                              <td></td>
+                              <td></td>
+                              <td></td>
+                              <td></td>
+                              <td></td>
+                              <td></td>
+                             <?php
+                    $no++;         
+                    }
+                             ?>
+                    </tr>
+          </table>
 </body>
 </html>
